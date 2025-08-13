ecb98ea1c90262f23795a23a62b26cfd6bd20a9e
Hemanth-c56 <hemanthceps@gmail.com>
2025-08-13 21:01:55 +0530

    email verification and pass resetting frontend pages structured

---
+++ frontend/next/src/app/(public)/(auth)/accounts/[reset-token]/reset-password/page.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { useState } from 'react';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Eye, EyeOff } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+
+import { Button } from '~/shared/shadcn/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '~/shared/shadcn/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '~/shared/shadcn/form';
+import { Input } from '~/shared/shadcn/input';
+
+const passwordSchema = z
+  .object({
+    newPassword: z.string().min(8, 'Password must be at least 8 characters long.'),
+    confirmPassword: z.string()
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: 'Password do not match.',
+    path: ['confirmPassword']
+  });
+
+type PasswordForm = z.infer<typeof passwordSchema>;
+
+export default function Page() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const form = useForm<PasswordForm>({
+    resolver: zodResolver(passwordSchema),
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      newPassword: '',
+      confirmPassword: ''
+    }
+  });
+
+  return (
+    <div className="flex w-full items-center justify-center p-6 lg:w-[40%]">
+      <Card className="w-full max-w-sm">
+        <CardHeader className="flex flex-col items-center text-center">
+          <CardTitle className="text-1xl font-bold md:text-2xl">Reset Your Password</CardTitle>
+          <CardDescription className="text-sm">
+            Enter and confirm your new password below.
+          </CardDescription>
+        </CardHeader>
+
+        <Form {...form}>
+          <form>
+            <CardContent className="flex flex-col gap-5">
+              {/* password  */}
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          id="newPassword"
+                          type={showPassword ? 'text' : 'password'}
+                          placeholder="8+ characters"
+                          required
+                          {...field}
+                        />
+                        <button
+                          type="button"
+                          onClick={() => setShowPassword((prev) => !prev)}
+                          className="absolute top-1/2 right-2 -translate-y-1/2 transform"
+                          tabIndex={-1}>
+                          {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                        </button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* confirm password */}
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          id="confirmPassword"
+                          type={showPassword ? 'text' : 'password'}
+                          placeholder="Match your above password"
+                          required
+                          {...field}
+                        />
+                        <button
+                          type="button"
+                          onClick={() => setShowPassword((prev) => !prev)}
+                          className="absolute top-1/2 right-2 -translate-y-1/2 transform"
+                          tabIndex={-1}>
+                          {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                        </button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardFooter className="flex flex-col gap-3">
+              <Button type="submit" className="w-full bg-[#6B0F1A] text-white hover:bg-[#44101b]">
+                Save New Password
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
+}
--- frontend/next/src/app/(public)/(auth)/accounts/email-verification/page.tsx
+++ frontend/next/src/app/(public)/(auth)/accounts/email-verification/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { REGEXP_ONLY_DIGITS_AND_CHARS } from 'input-otp';
+import { useState } from 'react';
 
 import { Button } from '~/shared/shadcn/button';
 import {
@@ -11,39 +11,40 @@ import {
   CardHeader,
   CardTitle
 } from '~/shared/shadcn/card';
-import { InputOTP, InputOTPGroup, InputOTPSlot } from '~/shared/shadcn/input-otp';
+import { Input } from '~/shared/shadcn/input';
+import { Label } from '~/shared/shadcn/label';
 
 export default function Page() {
+  const [email, setEmail] = useState('');
+
   return (
     <div className="flex w-full items-center justify-center p-6 lg:w-[40%]">
       <Card className="w-full max-w-sm">
         <CardHeader className="flex flex-col items-center text-center">
-          <CardTitle className="text-1xl font-bold md:text-2xl">OTP Verification</CardTitle>
+          <CardTitle className="text-1xl font-bold md:text-2xl">Email Verification</CardTitle>
           <CardDescription className="text-sm">
-            Please enter the code sent to your email to continue.
+            Please enter your registered email to continue.
           </CardDescription>
         </CardHeader>
         <CardContent className="flex justify-center">
-          <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
-            <InputOTPGroup>
-              <InputOTPSlot index={0} />
-              <InputOTPSlot index={1} />
-              <InputOTPSlot index={2} />
-              <InputOTPSlot index={3} />
-              <InputOTPSlot index={4} />
-              <InputOTPSlot index={5} />
-            </InputOTPGroup>
-          </InputOTP>
+          <div className="grid w-full gap-2.5">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="m@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
         </CardContent>
         <CardFooter className="flex flex-col gap-3">
           <Button type="submit" className="w-full bg-[#6B0F1A] text-white hover:bg-[#44101b]">
             Verify
           </Button>
           <div className="text-center text-sm">
-            Didn’t receive the code?{' '}
-            <a href="#" className="font-semibold underline-offset-4 hover:underline">
-              Resend OTP
-            </a>
+            A link to reset the password will be sent to you email after verification of the your
+            account.
           </div>
         </CardFooter>
       </Card>
