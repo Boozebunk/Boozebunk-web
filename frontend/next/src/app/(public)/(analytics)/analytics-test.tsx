ea10bcadc39b0743f99c869bb6b155c10dacf210
Hemanth-c56 <hemanthceps@gmail.com>
2025-12-07 15:51:43 +0530

    different route for analytics - debugging

---
+++ frontend/next/src/app/(public)/(analytics)/analytics-test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Analytics } from '~/components/admin-dashboard/analytics';
+
+function AnalyticsTest() {
+  return (
+    <div>
+      <Analytics />
+    </div>
+  );
+}
+
+export default AnalyticsTest;
